002cbd142e2a2a02a59ee8f03f58df1d94c35c13
Nikolay <pympyym98@mail.ru>
2021-09-29 02:40:42 +0700

    C#
    Version2.0

---
+++ ExtremumSearch/ExtremumSearch/GoldenRatio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExtremumSearch
+{
+    public class GoldenRatio
+    {
+        private double FI = (1 + Math.Sqrt(5)) / 2;
+        private double x1, x2;
+
+        public double GetExtremumMax(Func<double, double> Function, double a, double b, double eps){ //Точка максимума
+            while (Math.Abs(b - a) > eps)
+            {
+                x1 = b - (b - a) / FI;
+                x2 = a + (b - a) / FI;
+
+                if (Function(x1) > Function(x2))
+                    a = x1;
+                else
+                    b = x2;
+            }
+            return (a + b) / 2;
+        }
+
+        public double GetExtremumMin(Func<double, double> Function, double a, double b, double eps){ //Точка минимума
+            while (Math.Abs(b - a) > eps)
+            {
+                x1 = b - (b - a) / FI;
+                x2 = a + (b - a) / FI;
+
+                if (Function(x1) < Function(x2))
+                    a = x1;
+                else
+                    b = x2;
+            }
+            return (a + b) / 2;
+        }
+        //
+        public double DichotomyMethods(Func<double, double> Function, double a, double b, double eps) { //Методы дихотомии
+            double c;
+            while ((b - a) > eps)
+            {
+                c = (a + b) / 2;
+                if (Function(b) * Function(c) < 0)
+                    a = c;
+                else
+                    b = c;
+            }
+            return (a + b) / 2;
+        }
+    }
+}
---
+++ ExtremumSearch/ExtremumSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExtremumSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GoldenRatio gr = new GoldenRatio();
+
+            double eps = 0.0001; //Точность
+            double a = -2; //Начало отрезка
+            double b = 2; //Конец отрезка
+
+            double max = gr.GetExtremumMax(Function, a, b, eps);
+            double min = gr.GetExtremumMin(Function, a, b, eps);
+
+            double dm = gr.DichotomyMethods(Function, a, b, eps);
+
+            Console.WriteLine("Max: " + max + "  | Min: " + min + "  | Dichotomy: " + dm);
+
+            Console.ReadKey();
+        }
+
+        static double Function(double x){ //Здесь изменять функцию
+            return Math.Pow((x + 1), 3) + 5 * Math.Pow(x, 2);
+        }
+    }
+}
